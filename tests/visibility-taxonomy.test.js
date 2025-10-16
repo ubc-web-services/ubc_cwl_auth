8f9c8cae963676c34c0df0aba96cbbcff50d6eaa
emmahy-park <68018787+emmahy-park@users.noreply.github.com>
2025-10-16 14:28:51 -0700

    added tests to confirm CWL access role

--- tests/visibility-taxonomy.test.js
+++ tests/visibility-taxonomy.test.js
@@ -14,8 +14,26 @@ test.describe('Generic Test Suite - confirm visibility taxonomy exists', () => {
 
   test('Check visibility taxonomy has CWL and General terms', async ({ page }) => {
     await page.goto('/admin/structure/taxonomy/manage/visibility/overview');
-    await expect(page.locator('#taxonomy-overview-terms')).toContainText('CWL')
-    await expect(page.locator('#taxonomy-overview-terms')).toContainText('General')
+    await expect(page.locator('#taxonomy-overview-terms')).toContainText('CWL');
+    await expect(page.locator('#taxonomy-overview-terms')).toContainText('General');
+  });
+
+  test('Check CWL access role includes CWL', async ({ page }) => {
+    await page.goto('/taxonomy/term/2/edit?destination=/admin/structure/taxonomy/manage/visibility/overview');
+    await expect(page.locator('#block-claro-page-title')).toContainText('Edit term');
+
+    // Check permissions
+    await page.locator('[data-drupal-selector="edit-access"]').click();
+    await expect(page.locator('#edit-access-role-cwl')).toBeChecked();
+  });
+
+  test('Check General access role does not include CWL', async ({ page }) => {
+    await page.goto('/taxonomy/term/1/edit?destination=/admin/structure/taxonomy/manage/visibility/overview');
+    await expect(page.locator('#block-claro-page-title')).toContainText('Edit term');
+
+    // Check permissions
+    await page.locator('[data-drupal-selector="edit-access"]').click();
+    await expect(page.locator('#edit-access-role-cwl')).not.toBeChecked();
   });
 
 });
